71120477f9c685165756772535afd70741087dfd
Kevin <kevtehpro35@gmail.com>
2015-03-06 17:33:55 -0500

    Changed function names and added XOR and XNOR

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ var binaryReg = new RegExp("^[0-1]+$");
 var nameReg = new RegExp("^[a-zA-Z]$");
 
 $(document).ready(function() {
-    var canvas = document.getElementById("canvas");
-    var draw = canvas.getContext("2d");
-    canvas.width = 1000;
-    canvas.height = 500;
+    var binaryChart = document.getElementById("canvas");
+    var binaryDraw = binaryChart.getContext("2d");
+    binaryChart.width = 1000;
+    binaryChart.height = 500;
 
-    var answercanvas = document.getElementById("answerchart");
-    var answerdraw = answercanvas.getContext("2d"); 
-    answercanvas.width = 1000;
-    answercanvas.height = 500;
+    var expressionChart = document.getElementById("answerchart");
+    var expressionDraw = expressionChart.getContext("2d"); 
+    expressionChart.width = 1000;
+    expressionChart.height = 500;
 
+    // Textures prepared to draw onto canvas
     var low = new Image();
     low.src = "res/low.png";
     var high = new Image();
@@ -27,15 +28,15 @@ $(document).ready(function() {
 
     high.addEventListener("load", function(event) {
         setInterval(function() {
-            draw.clearRect(0, 0, canvas.width, canvas.height);
-            answerdraw.clearRect(0, 0, answercanvas.width, answercanvas.height);
+            binaryDraw.clearRect(0, 0, binaryChart.width, binaryChart.height);
+            expressionDraw.clearRect(0, 0, expressionChart.width, expressionChart.height);
             var expression = $("#answer").text();
             if(expression != "" && binaryReg.test(expression) == true) {
-                generateAnswerChart(expression);
+                generateExpressionChart(expression);
             }
             for(var i = 0; i != inputs; i++) {
                 if(sequences[String(Object.keys(sequences)[i])] != null) {
-                    generateChart(i);
+                    generateBinaryChart(i);
                 }
             }
             
@@ -97,7 +98,9 @@ $(document).ready(function() {
                     || proccessing.indexOf("00") != -1 || proccessing.indexOf("10") != -1
                     || proccessing.indexOf("01") != -1 || proccessing.indexOf("(0)") != -1
                     || proccessing.indexOf("(1)") != -1 || proccessing.indexOf("0'") != -1
-                    || proccessing.indexOf("1'") != -1 || proccessing.indexOf("0+1") != -1) {
+                    || proccessing.indexOf("1'") != -1 || proccessing.indexOf("0+1") != -1
+                    || proccessing.indexOf("1^1") != -1 || proccessing.indexOf("0^1") != -1
+                    || proccessing.indexOf("1^0") != -1 || proccessing.indexOf("0^0") != -1) {
                     if(proccessing.indexOf("1'") != -1) {
                         proccessing = proccessing.replace("1'", "0");
                     } else if(proccessing.indexOf("0'") != -1) {
@@ -133,6 +136,18 @@ $(document).ready(function() {
                     else if(proccessing.indexOf("0+1") != -1) {
                         proccessing = proccessing.replace("0+1", "1");
                     }
+                    else if(proccessing.indexOf("1^1") != -1) {
+                        proccessing = proccessing.replace("1^1", "0");
+                    }
+                    else if(proccessing.indexOf("0^1") != -1) {
+                        proccessing = proccessing.replace("0^1", "1");
+                    }
+                    else if(proccessing.indexOf("1^0") != -1) {
+                        proccessing = proccessing.replace("1^0", "1");
+                    }
+                    else if(proccessing.indexOf("0^0") != -1) {
+                        proccessing = proccessing.replace("0^0", "0");
+                    }
                 }
                 answer += proccessing;
             }
@@ -155,20 +170,20 @@ $(document).ready(function() {
         return small;
     }
 
-    function generateChart(j) {
+    function generateBinaryChart(j) {
         var sequence = String(sequences[Object.keys(sequences)[j]]);
         for(var i = 0; i != sequence.length; i ++) {
-            draw.drawImage(getTexture(sequence, i), 1 + (i * 32), j * 48);
+            binaryDraw.drawImage(getTextureFor(sequence, i), 1 + (i * 32), j * 48);
         }       
     }
 
-    function generateAnswerChart(expression) {
+    function generateExpressionChart(expression) {
         for(var i = 0; i != expression.length; i ++) {
-            answerdraw.drawImage(getTexture(expression, i), 1 + (i * 32), 0);
+            expressionDraw.drawImage(getTextureFor(expression, i), 1 + (i * 32), 0);
         }
     }
 
-    function getTexture(sequence, index) {
+    function getTextureFor(sequence, index) {
         var current = sequence.charAt(index);
         if(current == '1') {
             if(index == 0) {
